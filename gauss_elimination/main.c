dda1ccc7e0b92d56e22efaab528c5c262780dfd0
cjuniordev <carlosrogeriocr02@gmail.com>
2022-02-11 11:37:58 -0300

    feat: triangule matrix of m lines

--- gauss_elimination/main.c
+++ gauss_elimination/main.c
@@ -24,30 +24,48 @@ void augmentMatrix(int **A, int *b, int m, int n, int **Ab){
 }
 
 void gauss(int **Ab, int m, int n){
-    int k, m_pivot = 0, n_pivot = 0;
-    int pivot = Ab[m_pivot][n_pivot];
+    int k, m_pivot, n_pivot, pivot;
 
-    if(pivot != 0){
-        k = Ab[m_pivot+1][n_pivot] / pivot;
-        for(int i=0; i<n; i++){
-            Ab[m_pivot+1][i] = Ab[m_pivot+1][i] - (k * Ab[m_pivot][i]);
+    // for(int i=0; i<m; i++){
+        m_pivot = 0, n_pivot = 0;
+        pivot = Ab[m_pivot][n_pivot];
+        
+        
+        if(pivot != 0){
+            for(int l=1; l<m; l++){
+                k = Ab[m_pivot+l][n_pivot] / pivot;
+                for(int j=0; j<n; j++){
+                    Ab[m_pivot+l][j] = Ab[m_pivot+l][j] - (k * Ab[m_pivot][j]);
+                }
+            }
         }
-    }
+    //}
+    
 }
 
 int main(){
-    int row = 2, col = 2;
+    int row = 3, col = 3;
     int **A = (int **)malloc(row * sizeof(int*)), 
          *b = (int *)malloc(row * sizeof(int));    
 
     alocateMatrix(A, row, col);
 
+    // row 1
     A[0][0] = 1;
     A[0][1] = 3;
+    A[0][2] = 1;
+    // row 2
     A[1][0] = 4;
     A[1][1] = 4;
-    b[0] = 35;
-    b[1] = 60;
+    A[1][2] = 4;
+    // row 3 
+    A[2][0] = 3;
+    A[2][1] = 2;
+    A[2][2] = 6;
+    // b
+    b[0] = 37;
+    b[1] = 68;
+    b[2] = 47;
 
     int **augment = (int **)malloc(row * sizeof(int*));
     augmentMatrix(A, b, row, col, augment);
