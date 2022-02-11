9c9d45615b0192aa2e313d39b9392bf7f27e8270
cjuniordev <carlosrogeriocr02@gmail.com>
2022-02-11 10:30:49 -0300

    feat: augment matrix

--- gauss_elimination/main.c
+++ gauss_elimination/main.c
@@ -1,28 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void freeMatriz(int **M, int rows){
-    for(int i=0; i<rows; i++) free(M[i]);
+void freeMatriz(int **A, int m){
+    for(int i=0; i<m; i++) free(A[i]);
+}
+
+void augmentMatrix(int **A, int *b, int m, int n, int **Ab){
+    // A[m][n]
+    for(int i=0; i<m; i++){
+        Ab[i] = malloc((n+1) * sizeof(int));
+    }
+
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            Ab[i][j] = A[i][j];
+        }
+        Ab[i][n] = b[i];
+    }
 }
 
 int main(){
-    int col = 2;
-    int row = 2;
-    int **matriz = (int **)malloc(row * sizeof(int*));
+    int row = 2, col = 2;
+    int **A = (int **)malloc(row * sizeof(int*)), 
+         *b = (int *)malloc(row * sizeof(int));    
 
     for(int i=0; i<row; i++){
-        matriz[i] = malloc(col * sizeof(int));
+        A[i] = malloc(col * sizeof(int));
     }
 
+    A[0][0] = 1;
+    A[0][1] = 3;
+    A[1][0] = 4;
+    A[1][1] = 4;
+    b[0] = 35;
+    b[1] = 60;
+
+    int **augment = (int **)malloc(row * sizeof(int*));
+
+    augmentMatrix(A, b, row, col, augment);
+
     for(int i=0; i<row; i++){
-        for(int j=0; j<col; j++){
-            printf("%d\t", matriz[i][j]);
+        for(int j=0; j<=col; j++){
+            printf("%d\t", augment[i][j]);
         }
         printf("\n");
     }
 
-
-    freeMatriz(matriz, row);
+    freeMatriz(A, row);
+    freeMatriz(augment, row);
 
     return 0;
 }
